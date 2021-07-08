145508e1473c50dd1fd0fa44b814785e7f5ceefb
nickkauf <nkauf87@gmail.com>
2021-07-07 21:19:38 -0400

    first commit

---
+++ src/StringAssignment.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringAssignment {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String name;
+
+        System.out.print( "What is your name? ");
+        name = keyboard.next();
+        System.out.print( "Greetings, " + name);
+    }
+
+
+
+
+
+
+}
